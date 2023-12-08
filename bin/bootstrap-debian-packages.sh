e4a4737c93cc593fbd11d10651116d4f13d019b9
spencer <spencer@spencer.crooked.tcos.us>
2023-12-08 16:31:23 -0600

    added bootstrap debian

---
+++ bin/bootstrap-debian-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Spencer Butler <dev@tcos.us>
+# bootstrap a fresh debian base image
+
+[ ! $(id -u) = 0 ] && { echo "Use root."; exit 1; }
+
+# Disable suggestiongs and recommends
+cat > /etc/apt/apt.conf.d/01norecommend << EOF
+APT::Install-Recommends "0";
+APT::Install-Suggests "0";
+EOF
+
+packages="
+qemu-guest-agent
+neovim
+tmux
+npm
+jq
+ipython3
+ncat
+git
+"
+
+apt-get update
+apt-get install -y $packages
+apt-get clean
+
+# make nvim vim
+update-alternatives --install $(which vim) vim $(which nvim) 99
