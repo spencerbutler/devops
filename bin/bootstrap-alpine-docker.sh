551851538917cda372d7f41cd4969018ddb502a2
Spencer Butler <spencerb@honeycomb.net>
2023-12-23 20:18:52 -0600

    old edit

--- bin/bootstrap-alpine-docker.sh
+++ bin/bootstrap-alpine-docker.sh
@@ -2,6 +2,6 @@
 # simple bootstrap, from the template
 
 sudo apk update
--sudo apk add qemu-guest-agent curl vim tmux jq bash git ipython sudo nodejs bash-completion docker docker-bash-completion docker-cli-compose cloud-utils-growpart e2fsprogs-extra
+sudo apk add qemu-guest-agent curl vim tmux jq bash git ipython sudo nodejs bash-completion docker docker-bash-completion docker-cli-compose cloud-utils-growpart e2fsprogs-extra
 sudo rc-update add qemu-guest-agent py3-docker-py
 sudo rc-update add docker
