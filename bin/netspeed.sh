e5c729b3b0d3517e1dd073af1184b801360f5de1
spencer <spencer@docker-node-1.crooked-stage.tcos.us>
2023-12-24 09:32:41 -0600

    shellcheck pass netspeed.sh

--- bin/netspeed.sh
+++ bin/netspeed.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#
+# Pretty network speeds.
+
+TARGET_LIST='(eth[0-9] )'
+TARGET_LIST='(BMRU|MOPRU)'
 
 # find interfaces to watch
-targets=$(netstat -i | egrep '(BMRU|MOPRU)' | awk '{print $1'})
+targets=$(netstat -i | grep -E "$TARGET_LIST" | awk '{print $1}')
 unit=${unit:-B}
 
 delta() {
@@ -14,13 +19,13 @@ delta() {
 btok() {
     if [ $(( $1 / 10 )) -gt 0 ]; then
         unit=K
-        echo $1 | awk -v unit=$unit '{printf "%.3f%s", $1 / 1024, unit}'
+        echo "$1" | awk -v unit=$unit '{printf "%.3f%s", $1 / 1024, unit}'
     elif [ $(( $1 / 100 )) -gt 0 ]; then
         unit=M
-        echo $1 | awk -v unit=$unit '{printf "%.4f", $1 / 1024/1024, unit}'
+        echo "$1" | awk -v unit=$unit '{printf "%.4f", $1 / 1024/1024, unit}'
     else
         unit=B
-        echo $1 | awk -v unit=$unit '{printf "%d", $1, unit}'
+        echo "$1" | awk -v unit=$unit '{printf "%d", $1, unit}'
     fi
 }
 
@@ -30,18 +35,18 @@ while :; do
         if [ $x -ge 8 ]; then
             x=1
         fi
-        tx="$(netstat -i | grep ^$target | awk '{print $7}')"
-        rx=$(netstat -i | grep ^$target | awk '{print $3}')
+        tx="$(netstat -i | grep ^"$target" | awk '{print $7}')"
+        rx=$(netstat -i | grep ^"$target" | awk '{print $3}')
         sleep 1
-        tx2="$(netstat -i | grep ^$target | awk '{print $7}')"
-        rx2=$(netstat -i | grep ^$target | awk '{print $3}')
+        tx2="$(netstat -i | grep ^"$target" | awk '{print $7}')"
+        rx2=$(netstat -i | grep ^"$target" | awk '{print $3}')
     
-        rtx=$(btok $(delta $tx2 $tx))
-        rrx=$(btok $(delta $rx2 $rx))
+        rtx=$(btok "$(delta "$tx2" "$tx")")
+        rrx=$(btok "$(delta "$rx2" "$rx")")
     
         tput setab $x
         echo -e "IF: $target\t TX: ${rtx} ${unit}ps\t RX: ${rrx} ${unit}ps"
         tput sgr0
-        let x++
+        (( x++ )) || true
     done
 done
